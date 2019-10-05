02a48a6eeccd1ed2207851206c4361258fe655c3
Andrea S Barr <andreabarr87@gmail.com>
2019-10-05 20:07:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,4 +6,8 @@ def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts " #{board[6]} | #{board[7]} | #{board[8]} " 
 end
 
-print display_board
+puts display_board
+
+board = ["X", "x", " ", " ", " ", " ", " ", " ", " "]
+
+puts display_board
